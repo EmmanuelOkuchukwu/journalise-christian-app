d8c7526d5914a9208ff52fe87cbdaea838f7ebef
emmanz95 <emmanza2@gmail.com>
2021-07-24 23:52:54 +0100

    new changes

--- src/components/pages/Feed.jsx
+++ src/components/pages/Feed.jsx
@@ -6,6 +6,7 @@ import {AuthService} from "../../services/AuthenticationService";
 const Feed = () => {
     const [posts, setPosts] = useState([]);
     const [userInfo, setUserInfo] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         function fetchPosts() {
@@ -13,9 +14,11 @@ const Feed = () => {
                 .then((results) => {
                     setPosts(results);
                     console.log(results);
+                    setIsLoading(true);
                 })
                 .catch((error) => {
                     console.log(error);
+                    setIsLoading(false);
                 })
         }
         return fetchPosts();
@@ -40,19 +43,26 @@ const Feed = () => {
 
     return (
         <div className="feed">
-            <h2>Main Feed</h2>
-            <hr />
-            {posts?.posts?.length > 0 ? posts?.posts?.map(post => (
-                <div className="post-stack" key={post?._id}>
-                    <div className="feed-card">
-                        <div className="flex-header">
-                            <h3>{post?.title}</h3>
-                            {post.postedBy._id === userInfo.user._id && <i className="fas fa-trash" onClick={() => handleDelete(post._id)} />}
+            <div className="feed-heading">
+                <h2>Main Feed</h2>
+                <hr />
+            </div>
+            {isLoading ?
+                <div className="feed-section">
+                    {posts?.posts?.length > 0 ? posts?.posts?.map(post => (
+                        <div className="post-stack" key={post?._id}>
+                            <div className="feed-card">
+                                <div className="flex-header">
+                                    <h3>{post?.title}</h3>
+                                    {post.postedBy._id === userInfo.user._id && <i className="fas fa-trash" onClick={() => handleDelete(post._id)} />}
+                                </div>
+                                <img src={post?.photo} alt="" className="img-post" />
+                            </div>
                         </div>
-                        <img src={post?.photo} alt="" className="img-post" />
-                    </div>
-                </div>
-            )): <span>No posts found here</span>}
+                    )): <span>No posts found here</span>
+                    }
+                </div> : <span>Posts are loading...</span>
+            }
         </div>
     )
 }
